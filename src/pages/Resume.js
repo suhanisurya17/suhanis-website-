a23ffd265d0c1742c9bc1b5cb9d35ada8410cae0
Suhani Surya <suhanisurya17@gmail.com>
2025-12-05 14:04:19 -0500

    changing resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div style={{ height: '100%', padding: '1rem' }}>
       <iframe
-        src="/icons/Suhani Surya Summer 2026 Resume Software.pdf"
+        src="/icons/Suhani Surya PM Resume Summer 2026.pdf"
         title="Suhani Surya Resume"
         style={{
           width: '100%',
